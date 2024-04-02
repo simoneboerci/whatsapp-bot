ae7ff3ca5ee03c696d76cc7b03a3865f1ee60979
Simone Boerci <boercisimone@gmail.com>
2024-04-02 10:38:50 +0200

    Refactoring

--- scripts/Background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.includes("https://web.whatsapp.com") && changeInfo.status === 'complete') {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["contentScript.js"]
-      });
-    }
-  });
